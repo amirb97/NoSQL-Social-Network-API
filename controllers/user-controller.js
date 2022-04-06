d1f66508a3f04ea5713b04809adbf6873f33bf97
Era <amirb2467@gmail.com>
2022-04-05 18:35:51 -0700

    adding and removing friends implemented in api route, and user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -73,6 +73,34 @@ const userController = {
               res.json(dbUserData);
           })
           .catch(err => res.status(400).json(err));
+    },
+    
+    // add a friend to a user
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: getUserById( params.friendId )}},
+            { new: true, runValidators: true }
+        )
+          .then(dbUserData => {
+              if (!dbUserData) {
+                  res.status(400).json({ message: 'No user found with this id'});
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch(err => res.json(err));
+    },
+
+    // delete friend from user
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: { friendId: params.friendId }}},
+            { new: true }
+        )
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.json(err));
     }
 };
 
--- routes/api/user-route.js
+++ routes/api/user-route.js
@@ -4,11 +4,15 @@ const {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    removeFriend
 } = require('../../controllers/user-controller');
 
 router.route('/').get(getAllUsers).post(createUser);
 
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').put(addFriend).delete(removeFriend);
 
 module.exports = router;
